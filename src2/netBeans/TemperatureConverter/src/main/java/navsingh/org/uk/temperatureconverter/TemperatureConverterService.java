a511fc60eef0f853d441bf1a53295ed3921da4b3
Navjot Singh Virk <virksaabnavjot@gmail.com>
2016-11-15 00:55:15 +0000

    Add Java Class (Source Package)
    This will be the Service file

---
+++ src2/netBeans/TemperatureConverter/src/main/java/navsingh/org/uk/temperatureconverter/TemperatureConverterService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navsingh.org.uk.temperatureconverter;
+
+/**
+ *
+ * @author Navjot Singh Virk https://ie.linkedin.com/in/navjotvirk
+ * @Website: https://www.navsingh.org.uk
+ */
+public class TemperatureConverterService {
+    
+}
